94a016fa49d3252fa8a8ebf06ee9f1eff39d4e9b
DerIngo <21258298+DerIngo@users.noreply.github.com>
2023-03-01 15:37:08 +0100

    Test Klasse zum ... testen. Und als Beispiel.

---
+++ src/main/java/deringo/wisia/TestMain.java
@@ -0,0 +1,27 @@
+package deringo.wisia;
+
+import deringo.wisia.taxon.TaxonInformation;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+        int leereSeite = 1; // Beispiel für leere Seite
+        int testudoHermanni = 19442;
+        int chersinaAngulata = 2510;
+        int selenicereusTestudo = 30177; // Beispiel mit Fußnoten
+        
+        TaxonInformation taxonInformation;
+        taxonInformation = WisiaExtraktor.getTaxonInformation(leereSeite);
+        System.out.println(taxonInformation);
+        
+        taxonInformation = WisiaExtraktor.getTaxonInformation(testudoHermanni);
+        System.out.println(taxonInformation);
+        
+        taxonInformation = WisiaExtraktor.getTaxonInformation(chersinaAngulata);
+        System.out.println(taxonInformation);
+        
+        taxonInformation = WisiaExtraktor.getTaxonInformation(selenicereusTestudo);
+        System.out.println(taxonInformation);
+    }
+
+}
